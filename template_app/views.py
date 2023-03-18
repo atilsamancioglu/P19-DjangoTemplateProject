c326095f070d4fe62da6ca8e41f1376e72dcf585
Atıl Samancıoğlu <samancioglu.atil@gmail.com>
2023-03-18 13:33:50 +0300

    templates and jinja syntax examples

---
+++ template_app/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.urls import reverse
+
+# Create your views here.
+
+def index(request):
+    return render(request,"template_app/first.html")
+
+def weather_view(request):
+    weather_dictionary = {
+        "istanbul" : "30", 
+        "amsterdam" : "20",
+        "paris" : [5,14,20,21],
+        "rome" : {"morning" : 10, "evening" : 20},
+        "user_premium" : True,
+        }
+    return render(request,"template_app/weather.html",context=weather_dictionary)
